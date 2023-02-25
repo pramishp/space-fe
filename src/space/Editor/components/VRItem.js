cebf8530a0200f6539dea0ba9a8def0c91adc616
Nirajan Bekoju <nirajan.bekoju@gmail.com>
2023-02-25 13:24:11 +0545

    transformation in VR completed

--- src/space/Editor/components/VRItem.js
+++ src/space/Editor/components/VRItem.js
@@ -1,6 +1,6 @@
 import { RayGrab, Interactive, useController } from '@react-three/xr'
-import React, {useState} from 'react'
-
+import React, { useState, useRef } from 'react'
+import * as THREE from "three";
 
 
 const euclideanDistance = (point_1, point_2) => {
@@ -18,7 +18,27 @@ const calculateScale = (startDistance, endDistance, currentScaleVector) => {
     }
 }
 
+// const grabbingController = React.useRef<THREE.Object3D>()
+//   const groupRef = React.useRef<THREE.Group>(null!)
+//   const previousTransform = React.useMemo(() => new THREE.Matrix4(), [])
+//   React.useImperativeHandle(forwardedRef, () => groupRef.current)
+
+//   useFrame(() => {
+//     const controller = grabbingController.current
+//     const group = groupRef.current
+//     if (!controller) return
+
+//     group.applyMatrix4(previousTransform)
+//     group.applyMatrix4(controller.matrixWorld)
+//     group.updateMatrixWorld()
+
+//     previousTransform.copy(controller.matrixWorld).invert()
+//   })
+
 function VRItem(props) {
+    const meshInteractiveRef = useRef()
+    const previousTransform = React.useMemo(() => new THREE.Matrix4(), [])
+
     const leftController = useController("left");
     const rightController = useController("right");
 
@@ -26,12 +46,38 @@ function VRItem(props) {
     const [startDistance, setStartDistance] = useState(null);
     const [endDistance, setEndDistance] = useState(null);
 
-    const handleSelect = (event) => {
-        // console.log(event);
-        // props.onSelect()
-        // 
+    // set raygrab enabled
+    const [isRayGrabEnabled, setIsRayGrabEnabled] = useState(true);
+
+    const handleSelectStart = (event) => {
+        console.log("handle START : ", event);
+        setIsRayGrabEnabled(true)
     }
 
+
+
+    const handleSelectEnd = (event) => {
+        console.log("handle END end : ", event);
+        // after the select End, call the callback for position and rotaiton change
+        // setIsRayGrabEnabled(false);
+        const controller = event.target.controller;
+        // console.log("current mesh : ", controller);
+        props.onVRTransformReleased({ "uuid": props.uuid, "worldPosition": controller.position, "worldQuaternion": controller.quaternion });
+        console.log("mesh interactive : ", meshInteractiveRef);
+
+
+        // const group = meshInteractiveRef.current
+        // group.applyMatrix4(previousTransform)
+        // group.applyMatrix4(controller.matrixWorld)
+        // group.updateMatrixWorld()
+
+        // previousTransform.copy(controller.matrixWorld).invert()
+
+
+
+        console.log("after mesh interactive : ", meshInteractiveRef);
+        // setIsRayGrabEnabled(false);
+    }
     const handleSqueezeStart = (event) => {
         console.log("Squeeze started : ", event);
         // Make sure that both controller are on, else will cause error
@@ -40,6 +86,7 @@ function VRItem(props) {
     }
 
     const handleSqueezeEnd = (event) => {
+
         // console.log("Squeezing : ", event);
         // console.log("right Controller : ", rightController);
         // Make sure that both controller are on, else will cause error
@@ -51,25 +98,34 @@ function VRItem(props) {
 
         // set scaling of the object
         let scale = calculateScale(startDistance, endDistance, props.children.ref.current.scale);
-        props.children.ref.current.scale.x = scale;
-        props.children.ref.current.scale.y = scale;
-        props.children.ref.current.scale.z = scale;
+        console.log("scale : ", scale);
+        props.onObjectPropsChanged({ "uuid": props.uuid, "key": "scale", "val": scale });
+        // props.children.ref.current.scale.x = scale;
+        // props.children.ref.current.scale.y = scale;
+        // props.children.ref.current.scale.z = scale;
     }
 
-    console.log("props children : ", props.children);
+    // console.log("props children : ", props.children);
+
+    const children = <Interactive ref={meshInteractiveRef}
+        onSelectStart={(event) => handleSelectStart(event)}
+        onSelectEnd={(event) => handleSelectEnd(event)}
+        onSqueezeEnd={(event) => handleSqueezeEnd(event)}
+        onSqueezeStart={(event) => handleSqueezeStart(event)}
+    >
+        {props.children}
+    </Interactive>
+
+    if (isRayGrabEnabled) {
+        return (<RayGrab>
+            {children}
+        </RayGrab>)
+    }
 
 
     return (
         <>
-            <RayGrab>
-                <Interactive
-                    onSelect={(event) => handleSelect(event)}
-                    onSqueezeEnd={(event) => handleSqueezeEnd(event)}
-                    onSqueezeStart={(event) => handleSqueezeStart(event)}
-                >
-                    {props.children}
-                </Interactive>
-            </RayGrab>
+            {children}
         </>
     )
 }
