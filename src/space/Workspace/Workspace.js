63a81bfb5462a81bd121a0f33fba0388d222711f
pramishp <pramishp@users.noreply.github.com>
2023-03-23 17:59:13 +0545

    yjs initialization of scene data fixed

--- src/space/Workspace/Workspace.js
+++ src/space/Workspace/Workspace.js
@@ -357,13 +357,16 @@ function Workspace({ roomId, user }) {
     //TODO: Why console.log(initData) here is called 8 times ?
 
     // check sceneProps in initData
-    // if (Object.keys(initData.scene).length === 0){
-    //
-    //     const scene = {}
-    //     scene['light'] =  SCENE_PROPS_TYPES['light']
-    //     scene['color'] = SCENE_PROPS_TYPES['color']
-    //     initData.scene = scene;
-    // }
+    if (Object.keys(initData.scene).length === 0){
+        const scene = {}
+        scene['light'] =  SCENE_PROPS_TYPES['light']
+        scene['color'] = SCENE_PROPS_TYPES['color']
+        initData.scene = scene;
+
+        // write this to yjs
+        onInsertSceneProps({uuid:"light", val: scene['light']})
+        onInsertSceneProps({uuid:"color", val: scene['color']})
+    }
 
     return (
         <>
